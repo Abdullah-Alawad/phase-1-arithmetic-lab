45e2e8633db8136d7f6bfa3c6e096f718dadff7a
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-09-25 13:51:42 +0300

    Tests are done!

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 62;
+let num2 = 1;
+
+let multiply = num1 * num2;
+
+let random =Math.ceil(Math.random());
+
+let num3 = 4;
+let num4 = 10;
+let mod = num3 % num4;
+
+let max = Math.max(20, 3, 11, 15);
